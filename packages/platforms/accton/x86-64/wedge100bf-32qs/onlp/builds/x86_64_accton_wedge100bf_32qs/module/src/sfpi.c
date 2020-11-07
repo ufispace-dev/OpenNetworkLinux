66f5d94c6cd28c8dd1acb57db7394bc58222194d
Yi Tseng <yi@opennetworking.org>
2020-11-07 01:34:22 -0800

    Add Wedge100BF-32QS (#10)

---
+++ packages/platforms/accton/x86-64/wedge100bf-32qs/onlp/builds/x86_64_accton_wedge100bf_32qs/module/src/sfpi.c
@@ -0,0 +1,333 @@
+/************************************************************
+ * <bsn.cl fy=2014 v=onl>
+ *
+ *           Copyright 2014 Big Switch Networks, Inc.
+ *           Copyright 2016 Accton Technology Corporation.
+ *
+ * Licensed under the Eclipse Public License, Version 1.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *        http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ *
+ * </bsn.cl>
+ ************************************************************
+ *
+ *
+ *
+ ***********************************************************/
+#include <onlplib/i2c.h>
+#include <onlp/platformi/sfpi.h>
+#include <onlplib/file.h>
+#include "platform_lib.h"
+
+#include "x86_64_accton_wedge100bf_32qs_log.h"
+
+#define BIT(i)              (1 << (i))
+#define NUM_OF_SFP_PORT     32
+#define PORT_EEPROM_FORMAT  "/sys/bus/i2c/devices/%d-0050/eeprom"
+
+static const int sfp_bus_index[] = {
+    3,  2,  5,  4,  7,  6,  9, 8,
+    11,  10, 13, 12, 15, 14, 17, 16,
+    19, 18, 21, 20, 23, 22, 25, 24,
+    27, 26, 29, 28, 31, 30, 33, 32
+};
+
+static uint8_t onlp_sfpi_reg_val_to_port_sequence(uint8_t value, int revert)
+{
+    int i;
+    uint8_t ret = 0;
+
+    for (i = 0; i < 8; i++) {
+        if (i % 2) {
+            ret |= (value & BIT(i)) >> 1;
+        }
+        else {
+            ret |= (value & BIT(i)) << 1;
+        }
+    }
+
+    return revert ? ~ret : ret;
+}
+
+/**
+ * @brief Software initialization of the SFP module.
+ */
+int onlp_sfpi_sw_init(void) {
+    return ONLP_STATUS_OK;
+}
+
+/**
+ * @brief Hardware initialization of the SFP module.
+ * @param flags The hardware initialization flags.
+ */
+int onlp_sfpi_hw_init(uint32_t flags) {
+    return ONLP_STATUS_OK;
+}
+
+/**
+ * @brief Deinitialize the chassis software module.
+ * @note The primary purpose of this API is to properly
+ * deallocate any resources used by the module in order
+ * faciliate detection of real resouce leaks.
+ */
+int onlp_sfpi_sw_denit(void) {
+    return ONLP_STATUS_OK;
+}
+
+
+/**
+ * @brief Get the bitmap of SFP-capable port numbers.
+ * @param[out] bmap Receives the bitmap.
+ */
+int onlp_sfpi_bitmap_get(onlp_sfp_bitmap_t* bmap) {
+    /*
+     * Ports {0, 32}
+     */
+    int p;
+    AIM_BITMAP_CLR_ALL(bmap);
+
+    for(p = 0; p < NUM_OF_SFP_PORT; p++) {
+        AIM_BITMAP_SET(bmap, p);
+    }
+
+    return ONLP_STATUS_OK;
+}
+
+/**
+ * @brief Determine the SFP connector type.
+ * @param id The SFP Port ID.
+ * @param[out] rtype Receives the connector type.
+ */
+int onlp_sfpi_type_get(onlp_oid_id_t id, onlp_sfp_type_t* rtype) {
+    *rtype = ONLP_SFP_TYPE_QSFP28;
+    return ONLP_STATUS_OK;
+}
+
+/**
+ * @brief Determine if an SFP is present.
+ * @param id The SFP Port ID.
+ * @returns 1 if present
+ * @returns 0 if absent
+ * @returns An error condition.
+ */
+int onlp_sfpi_is_present(onlp_oid_id_t port) {
+    int present;
+    int bus = (port < 16) ? 36 : 37;
+    int addr = (port < 16) ? 0x22 : 0x23; /* pca9535 slave address */
+    int offset;
+
+    if (port < 8 || (port >= 16 && port <= 23)) {
+        offset = 0;
+    }
+    else {
+        offset = 1;
+    }
+
+    present = onlp_i2c_readb(bus, addr, offset, ONLP_I2C_F_FORCE);
+    if (present < 0) {
+        return ONLP_STATUS_E_INTERNAL;
+    }
+
+    present = onlp_sfpi_reg_val_to_port_sequence(present, 0);
+    return !(present & BIT(port % 8));
+}
+
+/**
+ * @brief Return the presence bitmap for all SFP ports.
+ * @param[out] dst Receives the presence bitmap.
+ */
+int onlp_sfpi_presence_bitmap_get(onlp_sfp_bitmap_t* dst) {
+    int i;
+    uint8_t bytes[4] = {0};
+
+    for (i = 0; i < AIM_ARRAYSIZE(bytes); i++) {
+        int bus = (i < 2) ? 36 : 37;
+        int addr = (i < 2) ? 0x22 : 0x23; /* pca9535 slave address */
+        int offset = (i % 2);
+
+        bytes[i] = onlp_i2c_readb(bus, addr, offset, ONLP_I2C_F_FORCE);
+        if (bytes[i] < 0) {
+            return ONLP_STATUS_E_INTERNAL;
+        }
+
+        bytes[i] = onlp_sfpi_reg_val_to_port_sequence(bytes[i], 1);
+    }
+
+    /* Convert to 32 bit integer in port order */
+    i = 0;
+    uint32_t presence_all = 0 ;
+    for(i = AIM_ARRAYSIZE(bytes)-1; i >= 0; i--) {
+        presence_all <<= 8;
+        presence_all |= bytes[i];
+    }
+
+    /* Populate bitmap */
+    for(i = 0; presence_all; i++) {
+        AIM_BITMAP_MOD(dst, i, (presence_all & 1));
+        presence_all >>= 1;
+    }
+
+    return ONLP_STATUS_OK;
+}
+
+/**
+ * @brief Return the RX_LOS bitmap for all SFP ports.
+ * @param[out] dst Receives the RX_LOS bitmap.
+ */
+int onlp_sfpi_rx_los_bitmap_get(onlp_sfp_bitmap_t* dst) {
+    return ONLP_STATUS_OK;
+}
+
+
+/**
+ * @brief Read bytes from the target device on the given SFP port.
+ * @param id The SFP Port ID.
+ * @param devaddr The device address.
+ * @param addr Read offset.
+ * @param[out] dst Receives the data.
+ * @param len Read length.
+ * @returns The number of bytes read or ONLP_STATUS_E_* no error.
+ */
+int onlp_sfpi_dev_read(onlp_oid_id_t id, int devaddr, int addr,
+                       uint8_t* dst, int len) {
+    int size = 0;
+    int bus = sfp_bus_index[id];
+    if(onlp_file_read(dst, len, &size, PORT_EEPROM_FORMAT, bus) != ONLP_STATUS_OK) {
+        AIM_LOG_ERROR("Unable to read eeprom from port(%d)\r\n", id);
+        return ONLP_STATUS_E_INTERNAL;
+    }
+
+    if (size != len) {
+        AIM_LOG_ERROR("Unable to read eeprom from port(%d), size(%d) is different!\r\n", id, size);
+        return ONLP_STATUS_E_INTERNAL;
+    }
+    return ONLP_STATUS_OK;
+}
+
+
+/**
+ * @brief Write bytes to the target device on the given SFP port.
+ * @param id The SFP Port ID.
+ * @param devaddr The device address.
+ * @param addr Write offset.
+ * @param src The bytes to write.
+ * @param len Write length.
+ */
+int onlp_sfpi_dev_write(onlp_oid_id_t id, int devaddr, int addr,
+                        uint8_t* src, int len) {
+    int bus = sfp_bus_index[id];
+    if(onlp_file_write(src, len, PORT_EEPROM_FORMAT, bus) != ONLP_STATUS_OK) {
+        AIM_LOG_ERROR("Unable to write into eeprom of that port(%d)\r\n", id);
+        return ONLP_STATUS_E_INTERNAL;
+    }
+    return ONLP_STATUS_OK;
+}
+
+/**
+ * @brief Read a byte from the target device on the given SFP port.
+ * @param id The SFP Port ID.
+ * @param devaddr The device address.
+ * @param addr The read address.
+ * @returns The byte on success or ONLP_STATUS_E* on error.
+ */
+int onlp_sfpi_dev_readb(onlp_oid_id_t id, int devaddr, int addr) {
+    int bus = sfp_bus_index[id];
+    return onlp_i2c_readb(bus, devaddr, addr, 0);
+}
+
+/**
+ * @brief Write a byte to the target device on the given SFP port.
+ * @param id The SFP Port ID.
+ * @param devaddr The device address.
+ * @param addr The write address.
+ * @param value The write value.
+ */
+int onlp_sfpi_dev_writeb(onlp_oid_id_t id, int devaddr, int addr,
+                         uint8_t value) {
+    int bus = sfp_bus_index[id];
+    onlp_i2c_writeb(bus, devaddr, addr, value, 0);
+    return ONLP_STATUS_OK;
+}
+
+/**
+ * @brief Read a word from the target device on the given SFP port.
+ * @param id The SFP Port ID.
+ * @param devaddr The device address.
+ * @param addr The read address.
+ * @returns The word if successful, ONLP_STATUS_E* on error.
+ */
+int onlp_sfpi_dev_readw(onlp_oid_id_t id, int devaddr, int addr) {
+    int bus = sfp_bus_index[id];
+    return onlp_i2c_readw(bus, devaddr, addr, 0);
+}
+
+/**
+ * @brief Write a word to the target device on the given SFP port.
+ * @param id The SFP Port ID.
+ * @param devaddr The device address.
+ * @param addr The write address.
+ * @param value The write value.
+ */
+int onlp_sfpi_dev_writew(onlp_oid_id_t id, int devaddr, int addr,
+                         uint16_t value) {
+    int bus = sfp_bus_index[id];
+    onlp_i2c_writew(bus, devaddr, addr, value, 0);
+    return ONLP_STATUS_OK;
+}
+
+/**
+ * @brief Perform any actions required after an SFP is inserted.
+ * @param id The SFP Port ID.
+ * @param info The SFF Module information structure.
+ * @note This function is optional. If your platform must
+ * adjust equalizer or preemphasis settings internally then
+ * this function should be implemented as the trigger.
+ */
+int onlp_sfpi_post_insert(onlp_oid_id_t id, sff_info_t* info) {
+    return ONLP_STATUS_OK;
+}
+
+/**
+ * @brief Get the SFP's OID header.
+ * @param id The SFP oid.
+ * @param [out] hdr Receives the header.
+ */
+int onlp_sfpi_hdr_get(onlp_oid_id_t id, onlp_oid_hdr_t* hdr) {
+    int is_present = onlp_sfp_is_present(id);
+    hdr->id = ONLP_SFP_ID_CREATE(id);
+    hdr->poid = 0;
+    hdr->status = is_present ? ONLP_OID_STATUS_FLAG_PRESENT : 0;
+    return ONLP_STATUS_OK;
+}
+
+/**
+ * @brief Set an SFP control.
+ * @param port The SFP Port ID.
+ * @param control The control.
+ * @param value The value.
+ */
+int onlp_sfpi_control_set(onlp_oid_id_t id, onlp_sfp_control_t control, int value)
+{
+    return ONLP_STATUS_E_UNSUPPORTED;
+}
+
+/**
+ * @brief Get an SFP control.
+ * @param port The SFP Port ID.
+ * @param control The control
+ * @param[out] value Receives the current value.
+ */
+int onlp_sfpi_control_get(onlp_oid_id_t id, onlp_sfp_control_t control, int* value)
+{
+    return ONLP_STATUS_E_UNSUPPORTED;
+}
+
