4b6543c426abf23b278a935ecc095e0e8bc901ca
fengkm <feng.cf.lee@ufispace.com>
2022-06-10 14:28:12 +0800

    [S9301-32DB][r1.0.5]  - [x] rename platform name from s9300-32db to s9301-32db  - [x] fix wrong psu type get and correct length in strncmp  - [x] add psu support list to identify psu type via part number

--- packages/platforms/ufispace/x86-64/s9300-32db/onlp/builds/x86_64_ufispace_s9300_32db/module/auto/make.mk
+++
@@ -1,7 +0,0 @@
-#
-# x86_64_ufispace_s9300_32db Autogeneration
-#
-###############################################################################
-x86_64_ufispace_s9300_32db_AUTO_DEFS := module/auto/x86_64_ufispace_s9300_32db.yml
-x86_64_ufispace_s9300_32db_AUTO_DIRS := module/inc/x86_64_ufispace_s9300_32db module/src
-include $(BUILDER)/auto.mk
---
+++ packages/platforms/ufispace/x86-64/s9301-32db/onlp/builds/x86_64_ufispace_s9301_32db/module/auto/make.mk
@@ -0,0 +1,7 @@
+#
+# x86_64_ufispace_s9301_32db Autogeneration
+#
+###############################################################################
+x86_64_ufispace_s9301_32db_AUTO_DEFS := module/auto/x86_64_ufispace_s9301_32db.yml
+x86_64_ufispace_s9301_32db_AUTO_DIRS := module/inc/x86_64_ufispace_s9301_32db module/src
+include $(BUILDER)/auto.mk
