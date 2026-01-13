68eb4f73f5b799609f5433df469766eba4b78af5
fengkm <chifeng.km@gmail.com>
2026-01-13 16:29:06 +0800

    sync from branch ufi-onlpv1-dev tag onlpv1-r6.1.2

---
+++ packages/platforms/ufispace/x86-64/s9620-40dg/onlp/builds/x86_64_ufispace_s9620_40dg/module/inc/x86_64_ufispace_s9620_40dg/x86_64_ufispace_s9620_40dg_dox.h
@@ -0,0 +1,26 @@
+/**************************************************************************//**
+ *
+ * x86_64_ufispace_s9620_40dg Doxygen Header
+ *
+ *****************************************************************************/
+#ifndef __x86_64_ufispace_s9620_40dg_DOX_H__
+#define __x86_64_ufispace_s9620_40dg_DOX_H__
+
+/**
+ * @defgroup x86_64_ufispace_s9620_40dg x86_64_ufispace_s9620_40dg - x86_64_ufispace_s9620_40dg Description
+ *
+
+The documentation overview for this module should go here.
+
+ *
+ * @{
+ *
+ * @defgroup x86_64_ufispace_s9620_40dg-x86_64_ufispace_s9620_40dg Public Interface
+ * @defgroup x86_64_ufispace_s9620_40dg-config Compile Time Configuration
+ * @defgroup x86_64_ufispace_s9620_40dg-porting Porting Macros
+ *
+ * @}
+ *
+ */
+
+#endif /* __x86_64_ufispace_s9620_40dg_DOX_H__ */
